3141aee0bfa50e98cfb6734e616b08ed333ff4c8
Oliver Vea <oliver.vea@gmail.com>
2024-12-26 21:24:05 +0900

    improved ResultProblem class with additional constructors and tests.

--- src/Olve.Utilities/Types/Results/ResultProblem.cs
+++ src/Olve.Utilities/Types/Results/ResultProblem.cs
@@ -1,10 +1,75 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Olve.Utilities.Types.Results;
 
 /// <summary>
 /// Represents a problem encountered during an operation.
 /// </summary>
 /// <param name="Message">The message describing the problem.</param>
-/// <param name="Args">Optional arguments providing additional details about the problem.</param>
 /// <param name="Tags">Optional tags categorizing the problem.</param>
 /// <param name="Severity">The severity level of the problem, where higher values indicate more severe problems.</param>
-public record ResultProblem(string Message, object[]? Args = null, string[]? Tags = null, int Severity = 0);
+/// <param name="Args">Optional arguments providing additional details about the problem.</param>
+public record ResultProblem(
+    [StringSyntax(StringSyntaxAttribute.CompositeFormat)] string Message,
+    string[] Tags,
+    int Severity,
+    object[] Args)
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResultProblem"/> class.
+    /// </summary>
+    /// <param name="message">The message describing the problem.</param>
+    public ResultProblem(string message)
+        : this(message, [], 0, []) { }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResultProblem"/> class.
+    /// </summary>
+    /// <param name="message">The message describing the problem.</param>
+    /// <param name="args">Optional arguments providing additional details about the problem.</param>
+    public ResultProblem([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string message, params object[] args)
+        : this(message, [], 0, args) { }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResultProblem"/> class.
+    /// </summary>
+    /// <param name="message">The message describing the problem.</param>
+    /// <param name="tags">Optional tags categorizing the problem.</param>
+    public ResultProblem(string message, string[] tags)
+        : this(message, tags, 0, []) { }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResultProblem"/> class.
+    /// </summary>
+    /// <param name="message">The message describing the problem.</param>
+    /// <param name="tags">Optional tags categorizing the problem.</param>
+    /// <param name="args">Optional arguments providing additional details about the problem.</param>
+    public ResultProblem([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string message, string[] tags, params object[] args)
+        : this(message, tags, 0, args) { }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResultProblem"/> class.
+    /// </summary>
+    /// <param name="message">The message describing the problem.</param>
+    /// <param name="severity">The severity level of the problem, where higher values indicate more severe problems.</param>
+    public ResultProblem(string message, int severity)
+        : this(message, [], severity, []) { }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResultProblem"/> class.
+    /// </summary>
+    /// <param name="message">The message describing the problem.</param>
+    /// <param name="severity">The severity level of the problem, where higher values indicate more severe problems.</param>
+    /// <param name="args">Optional arguments providing additional details about the problem.</param>
+    public ResultProblem([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string message, int severity, params object[] args)
+        : this(message, [], severity, args) { }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResultProblem"/> class.
+    /// </summary>
+    /// <param name="message">The message describing the problem.</param>
+    /// <param name="tags">Optional tags categorizing the problem.</param>
+    /// <param name="severity">The severity level of the problem, where higher values indicate more severe problems.</param>
+    public ResultProblem(string message, string[] tags, int severity)
+        : this(message, tags, severity, []) { }
+}
---
+++ tests/Olve.Utilities.Tests/Types/ResultTests.cs
@@ -0,0 +1,25 @@
+﻿using Olve.Utilities.Types.Results;
+
+namespace Olve.Utilities.Tests.Types;
+
+public class ResultTests
+{
+    public void Test()
+    {
+        const string message = "Hi!";
+        const int severity = 1;
+        var arg0 = new { x = 1 };
+        string[] tags = ["tag1", "tag2"];
+        
+        _ = new ResultProblem(message);
+        _ = new ResultProblem(message, severity);
+        _ = new ResultProblem(message, tags);
+        _ = new ResultProblem(message, tags, severity);
+        
+        // With arguments
+        _ = new ResultProblem(message + "{0}", arg0);
+        _ = new ResultProblem(message + "{0}", severity, arg0);
+        _ = new ResultProblem(message + "{0}", tags, arg0);
+        _ = new ResultProblem(message + "{0}", tags, severity, arg0);
+    }
+}
