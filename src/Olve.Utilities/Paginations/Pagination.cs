c70a7682499d563d224fad7af65605a84929dada
Oliver Vea <oliver.vea@gmail.com>
2025-01-11 10:46:28 +0900

    Adding Pagination.Offset

--- src/Olve.Utilities/Paginations/Pagination.cs
+++ src/Olve.Utilities/Paginations/Pagination.cs
@@ -5,4 +5,10 @@
 /// </summary>
 /// <param name="Page">The (0-based) page number.</param>
 /// <param name="PageSize">The number of items per page.</param>
-public sealed record Pagination(int Page, int PageSize);
+public readonly record struct Pagination(int Page, int PageSize)
+{
+    /// <summary>
+    /// The offset of the pagination.
+    /// </summary>
+    public int Offset => Page * PageSize;
+}
