a656a8ef04b49e725f6cc1a54c8dcd6fa57383f3
Oliver Vea <oliver.vea@gmail.com>
2025-01-14 12:49:00 +0900

    Added factories for creating instances of asynchronous operations and operations that implement specific interfaces.

---
+++ src/Olve.Utilities/Operations/AsyncOperationFactory`1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Olve.Utilities.Operations;
+
+/// <summary>
+/// A factory for creating instances of asynchronous operations that implement <see cref="IAsyncOperation{TRequest}"/>.
+/// </summary>
+/// <typeparam name="TOperation">The type of the asynchronous operation to create.</typeparam>
+/// <typeparam name="TRequest">The type of the request handled by the operation.</typeparam>
+/// <param name="serviceProvider">The <see cref="IServiceProvider"/> used to resolve dependencies for the operation.</param>
+public class AsyncOperationFactory<TOperation, TRequest>(IServiceProvider serviceProvider) where TOperation : IAsyncOperation<TRequest>
+{
+    /// <summary>
+    /// Creates a new instance of the specified asynchronous operation type.
+    /// </summary>
+    /// <returns>A new instance of <typeparamref name="TOperation"/>.</returns>
+    public TOperation Build() => serviceProvider.GetRequiredService<TOperation>();
+}
---
+++ src/Olve.Utilities/Operations/OperationFactory`1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Olve.Utilities.Operations;
+
+/// <summary>
+/// A factory for creating instances of operations that implement <see cref="IOperation{TRequest}"/>.
+/// </summary>
+/// <typeparam name="TOperation">The type of the operation to create.</typeparam>
+/// <typeparam name="TRequest">The type of the request handled by the operation.</typeparam>
+/// <param name="serviceProvider">The <see cref="IServiceProvider"/> used to resolve dependencies for the operation.</param>
+public class OperationFactory<TOperation, TRequest>(IServiceProvider serviceProvider) where TOperation : IOperation<TRequest>
+{
+    /// <summary>
+    /// Creates a new instance of the specified operation type.
+    /// </summary>
+    /// <returns>A new instance of <typeparamref name="TOperation"/>.</returns>
+    public TOperation Build() => serviceProvider.GetRequiredService<TOperation>();
+}
