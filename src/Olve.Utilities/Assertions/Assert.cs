6b69bfa04057cacc5a3fdccdc0e1877d8494d24a
Oliver Vea <oliver.vea@gmail.com>
2025-01-17 16:09:10 +0900

    feat: added `Assert.IsEmpty(IEnumerable<T>)` and `Assert.IsNotEmpty(IEnumerable<T>)`

--- src/Olve.Utilities/Assertions/Assert.cs
+++ src/Olve.Utilities/Assertions/Assert.cs
@@ -4,8 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace Olve.Utilities.Assertions;
 
 /// <summary>
-///     Provides a set of assertion methods:
-///     <li><see cref="That" /> method throws an <see cref="AssertionError" /> if the assertion is false.</li>
+///     Provides a set of assertion methods.
 /// </summary>
 public static class Assert
 {
@@ -36,6 +35,38 @@ public static class Assert
         where T : class
     {
         if (value is null)
+        {
+            throw new AssertionError(message);
+        }
+    }
+
+    /// <summary>
+    ///     Throws an <see cref="AssertionError" /> if the collection is not empty.
+    /// </summary>
+    /// <typeparam name="T">Type of elements in the collection.</typeparam>
+    /// <param name="collection">The collection to check.</param>
+    /// <param name="message">The message to include in the exception.</param>
+    /// <exception cref="AssertionError">Thrown if the collection is not empty.</exception>
+    [Conditional("DEBUG")]
+    public static void IsEmpty<T>(IEnumerable<T> collection, string message = "Collection should be empty.")
+    {
+        if (collection.Any())
+        {
+            throw new AssertionError(message);
+        }
+    }
+
+    /// <summary>
+    ///     Throws an <see cref="AssertionError" /> if the collection is empty.
+    /// </summary>
+    /// <typeparam name="T">Type of elements in the collection.</typeparam>
+    /// <param name="collection">The collection to check.</param>
+    /// <param name="message">The message to include in the exception.</param>
+    /// <exception cref="AssertionError">Thrown if the collection is empty.</exception>
+    [Conditional("DEBUG")]
+    public static void IsNotEmpty<T>(IEnumerable<T> collection, string message = "Collection should not be empty.")
+    {
+        if (!collection.Any())
         {
             throw new AssertionError(message);
         }
