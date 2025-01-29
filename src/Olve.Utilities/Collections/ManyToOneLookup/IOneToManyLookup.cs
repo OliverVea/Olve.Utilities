85e691752cfb29d9f778018782a249f21a5c671d
Oliver Vea <oliver.vea@gmail.com>
2025-01-29 16:13:21 +0900

    feat: adding `OneToManyLookup`

---
+++ src/Olve.Utilities/Collections/ManyToOneLookup/IOneToManyLookup.cs
@@ -0,0 +1,89 @@
+﻿namespace Olve.Utilities.Collections;
+
+/// <summary>
+///     Represents a one-to-many lookup between two sets of items.
+/// </summary>
+/// <typeparam name="TLeft">The type of the left-hand elements.</typeparam>
+/// <typeparam name="TRight">The type of the right-hand elements.</typeparam>
+public interface IOneToManyLookup<TLeft, TRight> : IEnumerable<KeyValuePair<TLeft, IReadOnlySet<TRight>>>
+    where TLeft : notnull
+    where TRight : notnull
+{
+    /// <summary>
+    ///     Gets all left-hand elements in the lookup.
+    /// </summary>
+    IEnumerable<TLeft> Lefts { get; }
+
+    /// <summary>
+    ///     Gets all right-hand elements in the lookup.
+    /// </summary>
+    IEnumerable<TRight> Rights { get; }
+
+    /// <summary>
+    ///     Determines whether the specified pair exists in the lookup.
+    /// </summary>
+    /// <param name="left">The left-hand element.</param>
+    /// <param name="right">The right-hand element.</param>
+    /// <returns><see langword="true" /> if the pair exists; otherwise, <see langword="false" />.</returns>
+    bool Contains(TLeft left, TRight right);
+
+    /// <summary>
+    ///     Gets the right-hand elements associated with the specified left-hand element.
+    /// </summary>
+    /// <param name="left">The left-hand element.</param>
+    /// <returns>A set of right-hand elements if found; otherwise, <see cref="NotFound" />.</returns>
+    OneOf<IReadOnlySet<TRight>, NotFound> Get(TLeft left);
+
+    /// <summary>
+    ///     Gets the left-hand element associated with the specified right-hand element.
+    /// </summary>
+    /// <param name="right">The right-hand element.</param>
+    /// <returns>The left-hand element if found; otherwise, <see cref="NotFound" />.</returns>
+    OneOf<TLeft, NotFound> Get(TRight right);
+
+    /// <summary>
+    ///     Sets the mapping for the specified left-hand element to the provided right-hand elements.
+    ///     Overwrites any existing mappings.
+    /// </summary>
+    /// <param name="left">The left-hand element.</param>
+    /// <param name="rights">The set of right-hand elements to associate.</param>
+    void Set(TLeft left, ISet<TRight> rights);
+
+    /// <summary>
+    ///     Adds or removes a pair to/from the lookup.
+    /// </summary>
+    /// <param name="left">The left-hand element.</param>
+    /// <param name="right">The right-hand element.</param>
+    /// <param name="value"><see langword="true" /> to add the pair; <see langword="false" /> to remove it.</param>
+    /// <returns>
+    ///     <see langword="true" /> if the operation made changes to the lookup; otherwise, <see langword="false" />.
+    /// </returns>
+    bool Set(TLeft left, TRight right, bool value);
+
+    /// <summary>
+    ///     Clears all mappings in the lookup.
+    /// </summary>
+    void Clear();
+
+    /// <summary>
+    ///     Removes all mappings for the specified left-hand element.
+    /// </summary>
+    /// <param name="left">The left-hand element.</param>
+    /// <returns><see langword="true" /> if mappings were removed; otherwise, <see langword="false" />.</returns>
+    bool Remove(TLeft left);
+
+    /// <summary>
+    ///     Removes the mapping for the specified right-hand element.
+    /// </summary>
+    /// <param name="right">The right-hand element.</param>
+    /// <returns><see langword="true" /> if the mapping was removed; otherwise, <see langword="false" />.</returns>
+    bool Remove(TRight right);
+
+    /// <summary>
+    ///     Removes the specified pair from the lookup.
+    /// </summary>
+    /// <param name="left">The left-hand element.</param>
+    /// <param name="right">The right-hand element.</param>
+    /// <returns><see langword="true" /> if the pair was removed; otherwise, <see langword="false" />.</returns>
+    bool Remove(TLeft left, TRight right);
+}
---
+++ src/Olve.Utilities/Collections/ManyToOneLookup/OneToManyLookup.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+
+namespace Olve.Utilities.Collections;
+
+/// <summary>
+/// Represents a one-to-many lookup between two sets of items.
+/// </summary>
+/// <typeparam name="TLeft">The type of the left-hand elements.</typeparam>
+/// <typeparam name="TRight">The type of the right-hand elements.</typeparam>
+public class OneToManyLookup<TLeft, TRight> : IOneToManyLookup<TLeft, TRight>
+    where TLeft : notnull
+    where TRight : notnull
+{
+    private readonly Dictionary<TLeft, HashSet<TRight>> _leftToRights;
+    private readonly Dictionary<TRight, TLeft> _rightToLeft;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OneToManyLookup{TLeft, TRight}"/> class.
+    /// </summary>
+    /// <param name="leftComparer">Comparer for left-hand elements.</param>
+    /// <param name="rightComparer">Comparer for right-hand elements.</param>
+    public OneToManyLookup(IEqualityComparer<TLeft>? leftComparer = null, IEqualityComparer<TRight>? rightComparer = null)
+    {
+        _leftToRights = new Dictionary<TLeft, HashSet<TRight>>(leftComparer);
+        _rightToLeft = new Dictionary<TRight, TLeft>(rightComparer);
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<TLeft> Lefts => _leftToRights.Keys;
+
+    /// <inheritdoc />
+    public IEnumerable<TRight> Rights => _rightToLeft.Keys;
+
+    /// <inheritdoc />
+    public bool Contains(TLeft left, TRight right) =>
+        _leftToRights.TryGetValue(left, out var rights) && rights.Contains(right);
+
+    /// <inheritdoc />
+    public OneOf<IReadOnlySet<TRight>, NotFound> Get(TLeft left) =>
+        _leftToRights.TryGetValue(left, out var rights) ? rights : new NotFound();
+
+    /// <inheritdoc />
+    public OneOf<TLeft, NotFound> Get(TRight right) =>
+        _rightToLeft.TryGetValue(right, out var left) ? left : new NotFound();
+
+    /// <inheritdoc />
+    public void Set(TLeft left, ISet<TRight> rights)
+    {
+        if (rights.Count == 0)
+        {
+            Remove(left);
+            return;
+        }
+
+        foreach (var right in rights)
+        {
+            if (_rightToLeft.TryGetValue(right, out var existingLeft) && !EqualityComparer<TLeft>.Default.Equals(existingLeft, left))
+            {
+                _leftToRights[existingLeft].Remove(right);
+            }
+            _rightToLeft[right] = left;
+        }
+        _leftToRights[left] = [..rights];
+    }
+
+    /// <inheritdoc />
+    public bool Set(TLeft left, TRight right, bool value)
+    {
+        if (value)
+        {
+            if (Contains(left, right)) return false;
+            if (_rightToLeft.TryGetValue(right, out var existingLeft))
+            {
+                _leftToRights[existingLeft].Remove(right);
+            }
+            _rightToLeft[right] = left;
+            if (!_leftToRights.TryGetValue(left, out var rights))
+            {
+                rights = new HashSet<TRight>();
+                _leftToRights[left] = rights;
+            }
+            rights.Add(right);
+            return true;
+        }
+        return Remove(left, right);
+    }
+
+    /// <inheritdoc />
+    public bool Remove(TLeft left)
+    {
+        if (!_leftToRights.TryGetValue(left, out var rights)) return false;
+        foreach (var right in rights)
+        {
+            _rightToLeft.Remove(right);
+        }
+        _leftToRights.Remove(left);
+        return true;
+    }
+
+    /// <inheritdoc />
+    public bool Remove(TRight right)
+    {
+        if (!_rightToLeft.Remove(right, out var left)) return false;
+
+        _leftToRights[left].Remove(right);
+
+        if (_leftToRights[left].Count == 0)
+        {
+            _leftToRights.Remove(left);
+        }
+
+        return true;
+    }
+
+    /// <inheritdoc />
+    public bool Remove(TLeft left, TRight right)
+    {
+        if (!_leftToRights.TryGetValue(left, out var rights) || !rights.Remove(right)) return false;
+        _rightToLeft.Remove(right);
+        if (rights.Count == 0) _leftToRights.Remove(left);
+        return true;
+    }
+
+    /// <inheritdoc />
+    public void Clear()
+    {
+        _leftToRights.Clear();
+        _rightToLeft.Clear();
+    }
+
+    /// <inheritdoc />
+    public IEnumerator<KeyValuePair<TLeft, IReadOnlySet<TRight>>> GetEnumerator() => _leftToRights.Select(pair => new KeyValuePair<TLeft, IReadOnlySet<TRight>>(pair.Key, pair.Value)).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
