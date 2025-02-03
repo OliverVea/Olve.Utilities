475ffbc57f2cac645bd3fa37b293a846ea5212f3
Oliver Vea <oliver.vea@gmail.com>
2025-02-03 17:03:28 +0900

    test: verifying that `ProjectRootHelper.ProjectRootFolder` has valid default on linux

---
+++ tests/Olve.Utilities.Tests/Projects/ProjectFolderHelperTests.cs
@@ -0,0 +1,32 @@
+using Olve.Utilities.Projects;
+
+namespace Olve.Utilities.Tests.Projects;
+
+public class ProjectFolderHelperTests
+{
+    [Test]
+    public async Task ProjectRootFolder_DefaultRootPath_ReturnsValidOSPath()
+    {
+        // Arrange
+        const string organization = "TestOrganization";
+        const string projectName = "TestProject";
+        var helper = new ProjectFolderHelper(organization: organization, projectName: projectName);
+        
+        // Act
+        var rootFolder = helper.ProjectRootFolder;
+        
+        // Assert
+        if (Environment.OSVersion.Platform == PlatformID.Unix)
+        {
+            await Assert.That(rootFolder).StartsWith("/home/");
+            var elements = rootFolder.Split("/").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+            await Assert.That(elements.Length).IsEqualTo(6);
+            await Assert.That(elements[0]).IsEqualTo("home");
+            await Assert.That(elements[1]).IsEqualTo(Environment.UserName);
+            await Assert.That(elements[2]).IsEqualTo(".local");
+            await Assert.That(elements[3]).IsEqualTo("share");
+            await Assert.That(elements[4]).IsEqualTo(organization);
+            await Assert.That(elements[5]).IsEqualTo(projectName);
+        }
+    }
+}
