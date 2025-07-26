8aa57d7551b97979d17a019f36a1be7272e83a9e
Oliver  Vea <oliver.vea@gmail.com>
2025-07-26 15:30:23 +0900

    feat: publish source generation analyzer

--- src/Olve.Validation.SourceGeneration/ValidatorForAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace Olve.Validation.SourceGeneration;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-public sealed class ValidatorForAttribute : Attribute
-{
-    public ValidatorForAttribute(Type targetType) => TargetType = targetType;
-
-    public Type TargetType { get; }
-}
