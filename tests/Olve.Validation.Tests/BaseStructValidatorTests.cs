69089cf993cfcdc742022d599cbe4b35b21bc759
Oliver Vea <oliver.vea@gmail.com>
2025-08-05 19:47:36 +0900

    feat(Olve.Validation): added new `WithMessage` and `WithProblem` methods for validators

--- tests/Olve.Validation.Tests/BaseStructValidatorTests.cs
+++ tests/Olve.Validation.Tests/BaseStructValidatorTests.cs
@@ -1,3 +1,5 @@
+using System;
+using Olve.Results;
 using Olve.Results.TUnit;
 using Olve.Validation.Validators.Base;
 
@@ -23,5 +25,51 @@ public class BaseStructValidatorTests
         await (expectedSuccess
             ? Assert.That(result).Succeeded()
             : Assert.That(result).Failed());
+    }
+
+    [Test]
+    public void WithProblem_NoPrevious_Throws()
+    {
+        var validator = new TestIntValidator();
+        Assert.Throws<InvalidOperationException>(() => validator.WithProblem(_ => new ResultProblem("msg")));
+    }
+
+    [Test]
+    public void WithProblem_AlreadyOverrode_Throws()
+    {
+        var validator = new TestIntValidator()
+            .NotDefault()
+            .WithMessage("first");
+        Assert.Throws<InvalidOperationException>(() => validator.WithProblem(_ => new ResultProblem("msg")));
+    }
+
+    [Test]
+    public async Task WithProblem_HappyPath_OverridesProblem()
+    {
+        var result = new TestIntValidator()
+            .NotDefault()
+            .WithProblem(_ => new ResultProblem("custom"))
+            .Validate(0);
+
+        await Assert.That(result)
+            .FailedAndProblemCollection()
+            .HasSingleItem()
+            .HasMember(x => x.Single().Message)
+            .EqualTo("custom");
+    }
+
+    [Test]
+    public async Task WithMessage_HappyPath_OverridesMessage()
+    {
+        var result = new TestIntValidator()
+            .NotDefault()
+            .WithMessage("custom msg")
+            .Validate(0);
+
+        await Assert.That(result)
+            .FailedAndProblemCollection()
+            .HasSingleItem()
+            .HasMember(x => x.Single().Message)
+            .EqualTo("custom msg");
     }
 }
